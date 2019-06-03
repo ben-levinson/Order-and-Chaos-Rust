b9c2c966704cd5244d8d9662f298f816aea46de9
lukas <lukasgross2020@u.northwestern.edu>
2019-06-02 21:43:18 -0500

    Working copy

--- src/strategy.rs
+++ src/strategy.rs
@@ -35,27 +35,47 @@ pub fn ai_move(game: &Game, player: Player) -> Game {
 }
 
 pub fn mini_max(game: &Game, player: Player) -> Option<Move> {
+    println!("Start minimax");
     if game.get_status() != GameStatus::InProgress {
         println!("Game not in progress");
         return None;
     }
     let mut best_move = None;
-    let mut best_score = -INFINITY;
+    let mut best_score = match player {
+        Player::Order => -INFINITY,
+        Player::Chaos => INFINITY,
+    };
     for (row, col) in game.open_indicies() {
         for &move_type in &[MoveType::X, MoveType::O] {
             let curr_move = Move::new(move_type, row, col);
             let curr_game = game.make_move(curr_move).unwrap();
-            let score = minimax_helper(1, curr_game, player);
-            if score > best_score {
-                best_score = score;
-                best_move = Some(curr_move);
+            //let score = minimax_helper(1, curr_game, player);
+            let score = alphabeta(curr_game, 3, -INFINITY, INFINITY, player);
+            if score.is_nan() {
+                continue;
+            }
+            dbg!(score);
+            match player {
+                Player::Order => {
+                    if score >= best_score {
+                        best_score = score;
+                        best_move = Some(curr_move);
+                    }
+                }
+                Player::Chaos => {
+                    if best_score >= score {
+                        best_score = score;
+                        best_move = Some(curr_move);
+                    }
+                }
             }
         }
     }
+    dbg!(best_move);
     best_move
 }
 
-fn minimax_helper(depth: usize, game: Game, player: Player) -> f64 {
+fn minimax_helper(depth: usize, game: &Game, player: Player) -> f64 {
     let eval = match player {
         Player::Order => order_eval,
         Player::Chaos => chaos_eval,
@@ -71,7 +91,7 @@ fn minimax_helper(depth: usize, game: Game, player: Player) -> f64 {
                 Some(g) => g,
                 None => continue,
             };
-            let score = minimax_helper(depth - 1, curr_game, player.other_player());
+            let score = minimax_helper(depth - 1, &curr_game, player.other_player());
             best_score = match player {
                 Player::Order => best_score.max(score),
                 Player::Chaos => best_score.min(score),
@@ -81,7 +101,52 @@ fn minimax_helper(depth: usize, game: Game, player: Player) -> f64 {
     return best_score;
 }
 
-fn order_eval(game: Game) -> f64 {
+fn alphabeta(game: Game, depth: usize, mut alpha: f64, mut beta: f64, player: Player) -> f64 {
+    if depth == 0 || game.get_status() != GameStatus::InProgress {
+        return order_eval(&game);
+    }
+    match player {
+        Player::Order => {
+            let mut value = -INFINITY;
+            for (row, col) in game.open_indicies() {
+                for &move_type in &[MoveType::X, MoveType::O] {
+                    let curr_move = Move::new(move_type, row, col);
+                    let next_game = game.make_move(curr_move).expect("Failed to make move");
+                    let new_val = alphabeta(next_game, depth - 1, alpha, beta, Player::Chaos);
+                    value = value.max(new_val);
+                    alpha = alpha.max(new_val);
+                    if alpha >= beta {
+                        return value;
+                    }
+                }
+            }
+            value
+        }
+        Player::Chaos => {
+            let mut value = INFINITY;
+            for (row, col) in game.open_indicies() {
+                for &move_type in &[MoveType::X, MoveType::O] {
+                    let curr_move = Move::new(move_type, row, col);
+                    let next_game = game.make_move(curr_move).expect("Failed to make move");
+                    let new_val = alphabeta(next_game, depth - 1, alpha, beta, Player::Order);
+                    value = value.min(new_val);
+                    beta = beta.min(new_val);
+                    if alpha >= beta {
+                        return value;
+                    }
+                }
+            }
+            value
+        }
+    }
+}
+
+fn order_eval(game: &Game) -> f64 {
+    let mut score = match game.get_status() {
+        GameStatus::OrderWins => return INFINITY,
+        GameStatus::ChaosWins => return -INFINITY,
+        GameStatus::InProgress => 0.,
+    };
     let (col, row) = game
         .last_move()
         .expect("Eval should never be called on an empty board");
@@ -91,10 +156,8 @@ fn order_eval(game: Game) -> f64 {
         game.count_direction(BoardDirection::Diagonal, row, col),
         game.count_direction(BoardDirection::AntiDiagonal, row, col),
     ];
-    let mut score = 0.;
     for count in counts {
         score += match count {
-            5 => INFINITY,
             4 => 25.,
             3 => 10.,
             2 => 5.,
@@ -104,23 +167,23 @@ fn order_eval(game: Game) -> f64 {
     score
 }
 
-fn chaos_eval(game: Game) -> f64 {
+fn chaos_eval(game: &Game) -> f64 {
     if game.get_status() == GameStatus::ChaosWins {
-        return INFINITY;
+        return -INFINITY;
     }
     let mut score = 0.;
     for (row, col) in game.open_indicies() {
         for &move_type in &[MoveType::X, MoveType::O] {
             let new_score = order_eval(
-                game.make_move(Move::new(move_type, row, col))
-                    .unwrap()
+                &game.make_move(Move::new(move_type, row, col))
+                     .unwrap()
             );
             if new_score > score {
                 score = new_score;
             }
         }
     }
-    -score
+    score
 }
 
 
@@ -137,26 +200,26 @@ mod minmax_tests {
 
         game = game.make_move(Move::new(x, 1, 0)).unwrap();
         assert_eq!(game.get_status(), GameStatus::InProgress);
-        score = order_eval(game.clone());
+        score = order_eval(&game);
         assert_eq!(score, 0.);
 
         game = game.make_move(Move::new(x, 2, 0)).unwrap();
         assert_eq!(game.get_status(), GameStatus::InProgress);
-        score = order_eval(game.clone());
+        score = order_eval(&game);
         assert_eq!(score, 5.);
 
         game = game.make_move(Move::new(x, 0, 1)).unwrap();
-        score = order_eval(game.clone());
+        score = order_eval(&game);
         assert_eq!(score, 0.);
 
         game = game.make_move(Move::new(x, 0, 2)).unwrap();
         assert_eq!(game.get_status(), GameStatus::InProgress);
-        score = order_eval(game.clone());
+        score = order_eval(&game);
         assert_eq!(score, 5.);
 
         game = game.make_move(Move::new(x, 0, 0)).unwrap();
         assert_eq!(game.get_status(), GameStatus::InProgress);
-        score = order_eval(game);
+        score = order_eval(&game);
         assert_eq!(score, 20.);
     }
 
